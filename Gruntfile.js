3104e880b781f7951efd487c8e0441870cea387c
Max Woo <maxithewoowoo@gmail.com>
2017-02-20 04:35:32 -0800

    added grunt task to combine+minify all src/ files

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+// combine src/ files
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      my_target : {
+        options : {
+          sourceMap : true, 
+          sourceMapName : 'dist/sourceMap.map'
+        },
+        files : {
+          'dist/cartesia.min.js' : [
+            'src/webgl-utils.js',
+            'src/Transformation.js',
+            'src/Mesh.js',
+            'src/WebGLWrapper.js',
+            'src/ShaderHandler.js',
+            'src/Cartesia.js',
+            'src/Camera.js',
+            'src/Cube.js',
+            'src/Sphere.js'
+          ]
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+};
